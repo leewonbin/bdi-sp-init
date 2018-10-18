ee1f492912fcfde95c627f1e4f429d5a668797c7
jeonas <jeonas@192.168.0.7>
2018-10-18 15:06:15 +0900

    1018/1506

--- src/main/java/com/bdi/sp/controller/UserController.java
+++ src/main/java/com/bdi/sp/controller/UserController.java
@@ -28,4 +28,21 @@ public class UserController {
 		}
 		return rMap;	
 	}
+	@RequestMapping(value="/login",method=RequestMethod.PUT)
+	public @ResponseBody Map<String,String> userUpdate(@RequestBody Map<String,String> user){
+		Map<String,String> rMap = new HashMap<String,String>();
+		System.out.println(user.get("id"));
+		rMap.put("수정", "fail");
+		rMap.put("msg", "아이디 및 비민번호를 확인하세요");
+		if(user.get("id")==null) {
+			System.out.println("널값이 나와요");
+			return rMap;
+		}else if(user.get("id").equals("hong")) {
+			System.out.println("수정이 왜 안될까?"+user.get("id"));
+				rMap.put("수정", "success");
+				rMap.put("msg", "수정되었습니다.");
+			
+		}
+		return rMap;	
+	}
 }
